eaac936139cd44b8dcad049118fa0e1960ef094f
Giovanni Camurati <camurati@eurecom.fr>
2021-08-13 11:53:41 +0200

    example with doppler correction

--- gnss/glonass/glonass-normal/generate-example.sh
+++ gnss/glonass/glonass-normal/generate-example.sh
@@ -1,3 +1,6 @@
 python3.6 ../glonass-sim.py signal --fs 1.5e6 --duration 5 -s 0 --sr 100 --cr 511000 -o /tmp/glonass.complex 
 python3.6 ../../../offline-noise-sdr/generate/rf-pwm.py generate --fs-in 1.5e6 --fs-out 3e6 --f-if 0.8750e6 --input-file /tmp/glonass.complex --output-file /tmp/glonass-example.rfpwm
+# reduce doppler offset
+python3.6 ../../../offline-noise-sdr/generate/rf-pwm.py generate --fs-in 1.5e6 --fs-out 3e6 --f-if 871500 --input-file /tmp/glonass.complex --output-file /tmp/glonass-example-offset.rfpwm
 adb push /tmp/glonass-example.rfpwm /data/local/tmp
+adb push /tmp/glonass-example-offset.rfpwm /data/local/tmp
